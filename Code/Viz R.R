c599c12ebb87332caf5292fa4808359c8a63769f
SethTheDataScientist <seth.lanza@gmail.com>
2023-10-18 17:57:34 -0400

    Another Day of Work
    Another Day of working on it. Not a ton of changes

--- Code/Viz R.R
+++ Code/Viz R.R
@@ -17,15 +17,16 @@ DefTeamTable <- FullTeamTable %>%
       (1 - percent_rank(BreakThroughD))
   )/3,
   Voronoi_Sum = MeanPlayD,
-  BreakThrough = BreakThroughD * 100
+  BreakThrough = BreakThroughD * 100,
+  possessionTeam2 = possessionTeam
   ) %>% 
-  select(possessionTeam, Voronoi_Sum, BreakThrough, KeepAway) %>% 
+  select(possessionTeam,possessionTeam2, Voronoi_Sum, BreakThrough, KeepAway) %>% 
   arrange(desc(KeepAway))
 
 # Define the color palette
 color_palette <- c("#e15759", "#edc948", "#59a14f")
 
-table = gt(DefTeamTable) %>% 
+gt(DefTeamTable) %>% 
   data_color(
     columns = c("Voronoi_Sum", "BreakThrough"),
     colors = scales::col_numeric(
@@ -50,7 +51,11 @@ table = gt(DefTeamTable) %>%
     title = md("Defensive Team Total Values"),
     subtitle = "Voronoi Weighted Sum of Area, Breakthrough Percentage, and KeepAway (Weighted Percentile of Voronoi and Breakthrough)") %>% 
   tab_source_note("Table: @SethDataScience | 2024 Big Data Bowl | Data: 2022 Weeks 1-9") %>% 
-gt_nfl_wordmarks(column ="possessionTeam", height = 30, locations = NULL)
+gt_nfl_wordmarks(column ="possessionTeam2", height = 30, locations = NULL)%>% 
+gt_nfl_logos(column ="possessionTeam", height = 30, locations = NULL)
+
+
+
 
 
 
@@ -165,4 +170,125 @@ table = gt(top_and_bottom_10) %>%
 
 gtsave(table, "LB Keepaway.png")
 
+
+
+#DATA VIZ ----------------------------------------------------------------
+# Gravity Plot ------------------------------------------------------------
+library(deldir)
+library(magick)
+library(ggforce)
+library(gganimate)
+library(transformr)
+
+AllGamesYAC <- read_csv("DataBowl/2024-Big-Data-Bowl/Created_DF/AllGamesYAC.csv")
+select_plays <- read_csv("DataBowl/2024-Big-Data-Bowl/Created_DF/viz_play.csv")
+
+# Field stuff -------------------------------------------------------------
+ScalingFactor = 2
+
+SelectPlay <- select_plays %>%
+  filter(club != "football",
+         frameId > 7) %>% 
+  mutate(x = if_else(is.na(x) == 1, 0, x * ScalingFactor),
+         y = if_else(is.na(y) == 1, 0, y * ScalingFactor),
+         HasBall = if_else(frameId >= 2,
+                           1,
+                           0.000000000000000001),
+         ballCarrier = if_else(nflId == 43454,
+                               HasBall * 100,
+                               0.000000000000001)) %>% 
+  group_by(frameId)
+  # mutate(
+  #   x_max = max(x)+1,
+  #   x_min = min(x)-1,
+  #   y_max = max(y)+1,
+  #   y_min = min(y)-1,
+  #   bound = cbind(c(y_min, y_max, y_max), c(x_min, x_min, x_max, x_max)))
+
+
+hash_right <- 38.35 * ScalingFactor
+hash_left <- 12 * ScalingFactor
+hash_width <- 3.3 * ScalingFactor
+# Define the range of coordinates
+xmin <- 0 * ScalingFactor
+xmax <- 53.3 * ScalingFactor
+ymin <- max(round(min(SelectPlay$x, 
+                      na.rm = TRUE) - (5 * ScalingFactor), -1), 0 * ScalingFactor)
+ymax <- min(round(max(SelectPlay$x, 
+                      na.rm = TRUE) + (5 * ScalingFactor), -1), 120 * ScalingFactor)
+
+df_hash <- 
+  expand.grid(x = c(0, 23.36667 * ScalingFactor, 29.96667 * ScalingFactor, xmax -  1 * ScalingFactor), 
+              y = (seq((10 * ScalingFactor), (110 * ScalingFactor), by = ScalingFactor))) %>% 
+  filter(!(floor(y %% (5 * ScalingFactor)) == 0), y < ymax, y > ymin)
+
+field_base <- ggplot() +
+  annotate("segment", x = df_hash$x[df_hash$x < 55/2 * ScalingFactor],
+           y = df_hash$y[df_hash$x < 55/2 * ScalingFactor],
+           xend = df_hash$x[df_hash$x < 55/2 * ScalingFactor] + 1 * ScalingFactor,
+           yend = df_hash$y[df_hash$x < 55/2 * ScalingFactor]) +
+  
+  annotate("segment", x = df_hash$x[df_hash$x > 55/2 * ScalingFactor],
+           y = df_hash$y[df_hash$x > 55/2 * ScalingFactor],
+           xend = df_hash$x[df_hash$x > 55/2 * ScalingFactor] + 1 * ScalingFactor,
+           yend = df_hash$y[df_hash$x > 55/2 * ScalingFactor]) +
+  
+  annotate("segment", x = xmin, y = seq(max(10 * ScalingFactor, ymin), min(ymax, 110 * ScalingFactor), by = 5 * ScalingFactor),
+           xend =  xmax, yend = seq(max(10 * ScalingFactor, ymin), min(ymax, 110 * ScalingFactor), by = 5 * ScalingFactor)) +
+  
+  annotate("text", x = rep(hash_left, 11), y = seq(10 * ScalingFactor, 110 * ScalingFactor, by = 10 * ScalingFactor),
+           label = c("G   ", seq(10, 50, by = 10), rev(seq(10, 40, by = 10)), "   G"), 
+           angle = 270, size = 5 * ScalingFactor) +
+  
+  annotate("text", x = rep((xmax - hash_left), 11), y = seq(10 * ScalingFactor, 110 * ScalingFactor, by = 10 * ScalingFactor),
+           label = c("   G", seq(10, 50, by = 10), rev(seq(10, 40, by = 10)), "G   "),
+           angle = 90, size = 5 * ScalingFactor) +
+  
+  annotate("segment", x = c(xmin, xmin, xmax, xmax), y = c(ymin, ymax, ymax, ymin),
+           xend = c(xmin, xmax, xmax, xmin), yend = c(ymax, ymax, ymin, ymin), color = "black")
+
+
+# Animated VIZ ---- 
+# 
+# triangle <- cbind(c(0, 53.3 * ScalingFactor, 53.3 * ScalingFactor, 0), c(60 * ScalingFactor, 60 * ScalingFactor, 90 * ScalingFactor, 90 * ScalingFactor))
+
+
+triangle <- cbind(c(xmin, xmax, xmax, xmin), c(ymin, ymin, ymax, ymax))
+
+play_animationVIZ = field_base+
+  geom_voronoi_tile(data = SelectPlay,
+                    aes(x = (xmax - y), y = x,
+                        fill = "#0e8326", group = -1,
+                        alpha = ballCarrier),
+                        bound = triangle)+
+  geom_voronoi_segment(data = SelectPlay,
+                    aes(x = (xmax - y), y = x),
+                    color = "black")+
+  geom_point(data = SelectPlay, 
+             aes(x = (xmax - y), y = x,
+                 group = nflId, 
+                 fill = primary,
+                 color = primary,
+                 ),
+             size = 7 * ScalingFactor) +
+  geom_text(data = SelectPlay,
+            aes(x = (xmax-y), y = x, label = jerseyNumber),
+            color = "white",
+                size = 3.5 * ScalingFactor,
+            vjust = 0.36 * ScalingFactor) +
+  scale_shape_manual(values = c(21, 16, 21), guide = FALSE) +
+  scale_fill_identity(aesthetics = c("fill", "color")) +
+  ylim(ymin, ymax)+
+  cowplot::theme_nothing() + theme(plot.title = element_text(),
+                                   plot.subtitle = element_text(),
+                                   plot.caption = element_text()) +
+  labs(caption = "@SethDataScience | Data: GameId = 2022102311, PlayId = 159")+
+  transition_time(SelectPlay$frameId) +
+  ease_aes('linear') + NULL
+
+
+ex_play_lengthVIZ <- length(unique(SelectPlay$frameId))
+
+
+animate(play_animationVIZ, fps = 25, nframe = ex_play_lengthVIZ, height = 1200, width = 1000)
 
